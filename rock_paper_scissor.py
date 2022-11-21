a99d6e6bed8e145b832d0e24ffb8576bc9a203c5
Hylyyy <palisochydeelyn97@gmail.com>
2022-11-21 22:24:15 +0800

    print play

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -12,8 +12,11 @@ def play():
     if user == computer:
         return "You and the computer have both chosen {}. It's a TIE!!!.".format(computer)
 
+    # r > s, s > p, p > r
     if is_win(user, computer):
         return "You have chosen {} and the computer has chosen {}. You WON!!!.".format(user, computer)
+
+    return "You have chosen {} and the computer has chosen {}. You LOST :{ !!!.".format(user, computer)
 
 def is_win(player, opponent):
     # if the player beats the opponent, return true.
@@ -22,4 +25,5 @@ def is_win(player, opponent):
         return True
     return False
 
-  
+if __name__ == '__main__':
+    print(play())
