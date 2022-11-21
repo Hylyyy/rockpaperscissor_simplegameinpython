672778af95a014a21334c326b35bac35dcba7789
Hylyyy <palisochydeelyn97@gmail.com>
2022-11-21 22:30:19 +0800

    import math

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -2,6 +2,7 @@ print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
 import random
+import math
 
 def play():
     user = input("What do you want to choose between 'r' for rock, 'p' for paper, and 's' for scissors?: ")
@@ -24,6 +25,14 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
     return False
+
+def play_best_of(n):
+    # will play continuosly agasint the computer until someone wins on the best of n games
+    # to win, one must win ceilin(n/2) games like 2/3, 3/5, 4/7
+    player_wins = 0
+    computer_wins = 0
+    wins_necessary = math.ceil(n/2)
+    print(wins_necessary)
 
 if __name__ == '__main__':
     print(play())
