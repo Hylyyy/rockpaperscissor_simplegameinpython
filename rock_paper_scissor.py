b3e1e31759ccb0aa68a9c759062dfa515e78f7bc
Hylyyy <palisochydeelyn97@gmail.com>
2022-11-21 21:59:07 +0800

    if tie and if user win

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,5 +1,20 @@
 print("************  PROGRAMMED BY  ************")
 print("********* HYDEE LYN C. PALISOC *********\n")
 
+import random
+
 def play():
-    user = input()
+    user = input("What do you want to choose between 'r' for rock, 'p' for paper, and 's' for scissors?: ")
+    user = user.lower()
+
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return "You and the computer have both chosen {}. It's a TIE!!!.".format(computer)
+
+    if is_win(user, computer):
+        return "You have chosen {} and the computer has chosen {}. You WON!!!.".format(user, computer)
+
+
+
+  
